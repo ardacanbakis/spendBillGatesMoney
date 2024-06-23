937f86ead36ad1f5bc8e108968be9ead1b69159b
Arda Canbakış <108702450+ardacanbakis@users.noreply.github.com>
2024-06-23 20:15:15 +0300

    minor ui fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,8 +4,8 @@ import "./header.css";
 const Header = ({ totalMoney }) => {
 
     return (
-        <div className={`totalmoney`}><a href="https://ardacanbakis.com" target="_blank">
-            <img src="https://neal.fun/spend/billgates.jpg" alt="Bill Gates"/></a>
+        <div className={`totalmoney`}>
+            <img src="https://neal.fun/spend/billgates.jpg" alt="Bill Gates"/>
             <h1>Spend Bill Gates' Money</h1>
             <span>${totalMoney}</span>
         </div>
